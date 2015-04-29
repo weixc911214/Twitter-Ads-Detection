088ebe6e588f832dcfa1ab869e29c730e40ea142
Xiaochen <weixc123@gmail.com>
2015-04-28 23:37:40 -0400

    fix promotion source bug

--- PromotionMonitor/PromotionExtractor.py
+++ PromotionMonitor/PromotionExtractor.py
@@ -25,6 +25,11 @@ if __name__ == "__main__":
             if len(context) > 0:
                 promoted_source = context[0].find(attrs={"class": "js-promoted-badge"}).text
 
+            creator = content.find_all(attrs={"class": "promoted-account-in-timeline-name"})
+            if len(creator) > 0:
+                promoted_source = creator[0].find(attrs={"class": "fullname"}).text
+
+
             # get the promotion image, if they have
             media = content.find_all(attrs={"class": "js-media-container"})
             img_url = ""
@@ -32,7 +37,7 @@ if __name__ == "__main__":
                 img_contents = media[0].find_all(attrs={"alt": "Embedded image permalink"})
                 if len(img_contents) > 0:
                     img_url = img_contents[0]['src']
-
+            print "-----"
             print "tweet: " + text
             print "promotion creator: " + promoted_source
             print "promotion image: " + img_url
