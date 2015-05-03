ede3bd57cc0eec2975bbd48b435904ece03b95c1
zjchen91 <zjchen91@gmail.com>
2015-05-03 18:22:48 -0400

    update GetKeywords

--- PromotionMonitor/GetKeywords.py
+++ PromotionMonitor/GetKeywords.py
@@ -1,26 +1,51 @@
 __author__ = 'jessie'
 
+import MySQLdb
 from alchemyapi import AlchemyAPI
 import json
 
+alchemyapi = AlchemyAPI()
 
-class GetKeywords():
-    def __init__(self, text):
-        alchemyapi = AlchemyAPI()
+db = MySQLdb.connect('cloud.comtnuycjpkv.us-west-2.rds.amazonaws.com', 'weixc1234', 'wxc16888', 'Twitter')
 
-        response = alchemyapi.keywords('text', text, {'sentiment': 1})
+cursor = db.cursor()
 
-        if response['status'] == 'OK':
-            for keyword in response['keywords']:
-                print('text: ', keyword['text'].encode('utf-8'))
-                print('relevance: ', keyword['relevance'])
-                print('')
-        else:
-            print('Error in : ', response['statusInfo'])
+cursor.execute('SELECT id, content FROM promotions')
+
+rows = cursor.fetchall()
+
+for row in rows:
+    response = alchemyapi.keywords('text', row[1], {'sentiment': 1})
+
+    if response['status'] == 'OK':
+        key = []
+        rel = []
+        count = 0
+        for keyword in response['keywords']:
+            key.append(keyword['text'].encode('utf-8'))
+            rel.append(keyword['relevance'])
+            count = count + 1
+
+
+        # Prepare SQL query to INSERT a record into the database.
+        try:
+           # Execute the SQL command
+           cursor.execute("""UPDATE Twitter.promotions SET keyword1 = %s, relevance1 = %s,keyword2 = %s, relevance2 = %s,keyword3 = %s, relevance3 = %s WHERE id = %s""",(key[0],rel[0],key[1],rel[1],key[2],rel[2],row[0]))
+           print 'ok'
+           # Commit your changes in the database
+           db.commit()
+        except:
+           # Rollback in case there is any error
+           db.rollback()
+    else:
+        print('Error in : ', response['statusInfo'])
+
+
+# disconnect from server
+db.close()
 
 
 
-GetKeywords('Bob breaks my fancy iphone and I have to send it to Apple store to repair.')
 
 
 
